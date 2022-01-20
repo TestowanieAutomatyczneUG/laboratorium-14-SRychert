646344d207eada8b9c1c525e1a4ace9d31918ed4
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-01-20 17:24:20 +0100

    zad1

--- zad1/features/steps/roman.py
+++ zad1/features/steps/roman.py
@@ -3,14 +3,17 @@ from src.roman import roman
 
 use_step_matcher('parse')
 
-@given("there is roman")
+
+@given(u'there is roman')
 def step_impl(context):
     context.roman = roman
 
-@when("the given number is {number}")
+
+@when(u'the given number is "{number}"')
 def step_impl(context, number):
     context.result = context.roman(int(number))
 
-@then("the result is {result}")
+
+@then(u'the result is "{result}"')
 def step_impl(context, result):
     assert context.result == result
